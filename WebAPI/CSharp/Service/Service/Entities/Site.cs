4c44cf33a869715c48141f23519e341d0bb54090
Guardien <guardien@mail.ru>
2016-06-25 15:24:30 +0300

    Созданы сущности. Создан веб контроллер

---
+++ WebAPI/CSharp/Service/Service/Entities/Site.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Service.Entities
+{
+    public class Site
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+    }
+}
