efddb0c0096474f3aed0d78ed3551657985607e0
Alexander Shvaykin <skiline.alex@gmail.com>
2016-06-28 20:45:26 +0300

    added method general_rank for person and add bootstarp js in app

--- web_ui/ruby/app/models/person.rb
+++ web_ui/ruby/app/models/person.rb
@@ -3,4 +3,10 @@ class Person < ActiveRecord::Base
   has_many :person_page_rank
 
   validates(:name, presence: true)
+
+  def general_rank
+    rank_sum = 0
+    person_page_rank.find_each(batch_size: 100){|rank_page| sum+= rank_page.rank}
+    sum
+  end
 end
