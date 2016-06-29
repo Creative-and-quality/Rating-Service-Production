051ad53af66183be475451baf2314bbc036f7d34
Alexander Shvaykin <skiline.alex@gmail.com>
2016-06-30 00:55:37 +0300

    added output of persons on the general statistics page

--- web_ui/ruby/app/controllers/general_statistics_controller.rb
+++ web_ui/ruby/app/controllers/general_statistics_controller.rb
@@ -1,4 +1,13 @@
 class GeneralStatisticsController < ApplicationController
   def index
+    @sites = sites_list
+    @last_site = Site.find(option_site_id_params) if params[:option_site_id]
+    @persons = Person.find_each if @last_site
+  end
+
+  private
+
+  def option_site_id_params
+    params.require(:option_site_id)
   end
 end
