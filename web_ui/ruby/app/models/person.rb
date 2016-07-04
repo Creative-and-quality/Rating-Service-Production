d647256717e498aae6e440f565641f77f8cd2020
Alexander Shvaykin <skiline.alex@gmail.com>
2016-07-04 20:42:38 +0300

    finish proto version general statistic

--- web_ui/ruby/app/models/person.rb
+++ web_ui/ruby/app/models/person.rb
@@ -4,14 +4,6 @@ class Person < ActiveRecord::Base
 
   validates(:name, presence: true)
 
-  def summ_general_rank_on_site(site_id)
-    rank_sum = 0
-    general_rank_pages_on_site(site_id).each{|rank_page| rank_sum+= rank_page.rank}
-    rank_sum
-  end
-
-  private
-
   def general_rank_pages_on_site(site_id)
     page_ids = "SELECT id FROM pages
     WHERE  site_id = :site_id"
