137c421645e6a1580461da41ab8ac07d752f6ece
Alexander Shvaykin <skiline.alex@gmail.com>
2016-07-15 01:19:10 +0300

    finish person controller

--- web_ui/ruby/app/controllers/api/keyword_controller.rb
+++ web_ui/ruby/app/controllers/api/keyword_controller.rb
@@ -7,50 +7,55 @@ class Api::KeywordController < Api::ApplicationController
     render :json => @keywords
   end
 
+  def create
+    @person = Person.find_by(id: params['person_id'])
+    @keyword = @person.keyword.new(Name: params['Name']) if @person
+
+    render :json => response_body_create
+
+  end
+
   def show
-    @keyword = Keyword.find_by id: params['id']
+    @keyword = Keyword.find_by(id: params['id'])
     if @keyword
       render :json => @keyword
     else
       render :json => response_not_found
-    end
-
-  end
-
-  def create
-    @person = Person.find_by(id: params['person_id'])
-
-    if @person
-      @keyword = @person.keyword.new(Name: params['Name'])
-    else
-      render :json => response_not_found
-    end
-
-    if @keyword.save
-      render :json => @keyword
-    else
-      render :json => @keyword.errors
     end
 
   end
 
   def update
-    @keyword = Keyword.find_by id: params['id']
-    if @keyword
-      @keyword.update_attributes(@keyword_params)
+    @keyword = Keyword.find_by(id: params['id'])
+
+    if @keyword && @keyword.update_attributes(@name_params)
       render :json => @keyword
-    else
+    elsif @keyword.nil?
       render :json => response_not_found
+    else
+      render :json => @keyword.errors
     end
   end
 
   def destroy
-    @keyword = Keyword.find_by id: params['id']
+    @keyword = Keyword.find_by(id: params['id'])
     if @keyword
       @keyword.destroy!
       render :json => response_ok
     else
       render :json => response_not_found
+    end
+  end
+  
+  private
+
+  def response_body_create
+    if @keyword && @keyword.save
+      @keyword
+    elsif @person.nil?
+      response_not_found
+    else
+      @keyword.errors
     end
   end
 
--- web_ui/ruby/app/controllers/api/person_controller.rb
+++ web_ui/ruby/app/controllers/api/person_controller.rb
@@ -14,4 +14,36 @@ class Api::PersonController < Api::ApplicationController
       render :json => @person.errors
     end
   end
+
+  def show
+    @person = Person.find_by(id: params['id'])
+    if @person
+      render :json => @person
+    else
+      render :json => response_not_found
+    end
+  end
+
+  def update
+    @person = Person.find_by(id: params['id'])
+
+    if @person && @person.update_attributes(@name_params)
+      render :json => @person
+    elsif @person.nil?
+      render :json => response_not_found
+    else
+      render :json => @person.errors
+    end
+  end
+
+  def destroy
+    @person = Person.find_by(id: params['id'])
+    if @person
+      @person.destroy!
+      render :json => response_ok
+    else
+      render :json => response_not_found
+    end
+
+  end
 end
