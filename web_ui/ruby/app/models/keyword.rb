18dc469aa832588fc71be4e3e20a91881f501a9c
Alexander Shvaykin <skiline.alex@gmail.com>
2016-07-14 23:39:12 +0300

    add validate keyword model

--- web_ui/ruby/app/models/keyword.rb
+++ web_ui/ruby/app/models/keyword.rb
@@ -1,6 +1,7 @@
 class Keyword < ActiveRecord::Base
   self.table_name = "Keywords"
-  validates(:Name, presence: true)
+  validates  :Name, presence: true, length: { maximum: 50 }
+  
   belongs_to :person, primary_key: 'PersonID'
 
   include Models::ColumnMethodsMysql
